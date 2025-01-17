8e93cb826c2295eee00de02e9d092296f8d8f22d
cascading-jox <cascading.jox@gmail.com>
2025-01-17 13:20:32 +0100

    ci: update husky

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,21 +1,3 @@
 export default {
     extends: ['@commitlint/config-conventional'],
-    rules: {
-        'type-enum': [
-            2,
-            'always',
-            [
-                'feat', // New feature
-                'fix', // Bug fix
-                'docs', // Documentation
-                'style', // Code style changes
-                'refactor', // Code refactoring
-                'perf', // Performance improvements
-                'test', // Tests
-                'chore', // Maintenance
-                'revert', // Revert changes
-                'release', // Release version
-            ],
-        ],
-    },
 };
